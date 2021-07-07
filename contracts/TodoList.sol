8d363a6363d6fe98ffe66b408eeedbe4837bb4ee
Hayt <vell.romas@gmail.com>
2021-07-06 19:40:34 -0500

    Finished setup and starting functionality

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public task;
+
+constructor() public {
+  createTask("First Task Test");
+}
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    task[taskCount] = Task(taskCount, _content, false);
+  }
 }
